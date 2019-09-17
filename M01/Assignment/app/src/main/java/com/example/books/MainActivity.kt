2b4bf4a0de7cb22101281e62cca65306eeb42fc5
Albert Yakubov <mrayakubov@hotmail.com>
2019-09-16 21:27:34 -0600

    Update MainActivity.kt
    appers I have fixed the bug with passing intents that was making the app crash. I was not passing a companion object which was a mistake
    
    Co-Authored-By: Basil <heiligbasil@users.noreply.github.com>

--- M01/Assignment/app/src/main/java/com/example/books/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/example/books/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.example.books
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.books.BookData.Companion.createBookEntry
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val NEW_ENTRY_REQUEST = 2
+        const val EDIT_ENTRY_REQUEST = 1
+    }
+
 
     private var entryList = mutableListOf<Book>()
 
@@ -15,9 +23,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button.setOnClickListener { view ->
+            val intent = Intent(this@MainActivity, EditBookActivity::class.java)
+            val entry = createBookEntry()
+            intent.putExtra(Book.TAG, entry)
+            startActivityForResult(intent, NEW_ENTRY_REQUEST)
+        }
+
 
     }
-
 
         private fun createBookEntry(entry: Book): TextView {
 
@@ -30,9 +44,8 @@ class MainActivity : AppCompatActivity() {
             view.textSize = 22f
             return view
 
-
         }
-
-
     }
+
+
 
